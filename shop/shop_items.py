7b5e4656b4c3be89b0bcf2eaaa8f2c1acb30ba32
DoKaUni <dovydas.kasulis@gmail.com>
2024-04-16 17:41:06 +0300

    Trečia užduotis

--- shop/shop_items.py
+++ shop/shop_items.py
@@ -15,6 +15,7 @@
         
     Rezultatas:
         Programa sukuria prekių objektus ir gali atlikti visas ankščiau pabrėžtas funkcijas.
+
 """
 
 class Item:
@@ -27,7 +28,7 @@ class Item:
         if not isinstance(quantity, int): # Tikrinama ar kiekis yra sveikas skaičius
             raise TypeError("Prekės kiekis yra netinkamo tipo")
         elif quantity < 0: # Tikrinama ar kiekis nėra mažesnis už vieną
-            raise ValueError("Prekės kiekis negali būti mažiau negų vienas")
+            raise ValueError("Prekės kiekis negali būti neigiamas")
         
         if not isinstance(price, float) and not isinstance(price, int): # Tikrinama ar kaina yra skaičius
             raise TypeError("Prekės kaina yra netinkamo tipo")
@@ -50,3 +51,34 @@ class Item:
                 "price": self.price,
                 "total_price" : self.get_total_price()
                 }
+
+"""
+    Užduotis #3
+    Programa turi klases maisto ir gėrimo prekėms. Šios prekės paveldi viska iš prekės (Item) klasės, taigi turi tuos pačius parametrus
+    ir funckijas, iškyrus viena - full_info(). full_info() yra redaguota tarp abudvieju klasiu, kad nurodytu ar prekė yra maistas ar
+    gėrimas.
+
+    Parametrai:
+        Jokių naujų parametru nėra, taigi visi yra tie patys, kaip ir prekės (Item) klasėje.
+
+        name (string): tekstas, kuris nurodo prekės pavadinimą.
+        quantity (int): skaičius, kuris nurodo prekės kiekį.
+        price (int): skaičius, kuris prekės vieneto kainą.
+        
+    Rezultatas:
+        Su programa galima sukurti maisto ir gėrimų prekių objektus ir gali atlikti visas prekės funkcijas.
+"""
+
+class Food(Item): # Maisto (Food) klasė, kuri paveldi prekės (Item) klasę
+    def __init__(self, name, quantity, price):
+        super().__init__(name, quantity, price) # Paveldima viskas iš prekės (Item) klasės
+    
+    def full_info(self): # Sukuria ir gražina tekstą sudarytą iš maisto prekės duomenų
+        return "Maistas {} {} {} {}".format(self.name, self.price, self.quantity, self.get_total_price())
+
+class Drink(Item): # Gėrimo (Drink) klasė, kuri paveldi prekės (Item) klasę
+    def __init__(self, name, quantity, price):
+        super().__init__(name, quantity, price) # Paveldima viskas iš prekės (Item) klasės
+
+    def full_info(self): # Sukuria ir gražina tekstą sudarytą iš gėrimo prekės duomenų
+        return "Gėrimas {} {} {} {}".format(self.name, self.price, self.quantity, self.get_total_price())
